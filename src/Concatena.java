38f8dd0bad77f26bacaeed80fd6c349b997687fc
Nachogarvi <nachogarvi10@gmail.com>
2025-12-09 18:48:10 +0100

    Cambio saludo

--- src/Concatena.java
+++ src/Concatena.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -10,9 +10,9 @@
 public class Concatena {
 
     public static void main(String[] args) {
-        String a = "hola";
+        String a = "Buenos dias";
         String b = " adios";
-        String valor = "hola";
+        String valor = "Buenos días";
 
         valor = valor + " y hasta luego";
         valor += " y adiós";
